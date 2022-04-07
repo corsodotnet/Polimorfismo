95d7ebbb7e135d381cb49b9ada86284beae389d0
Bruno Ferreira Rocha <bruno_ferreira@hotmail.it>
2022-04-07 10:45:16 +0200

    Add project files.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Person person = new Cliente();
+            person.getData();
+        }
+
+    }
+    public class Person
+    {
+        public string name;
+        public int age;
+
+        public virtual void getData()
+        {
+
+        }
+
+        public Person()
+        {
+
+        }
+    }
+
+    public class Cliente : Person
+    {
+        public string codiceCliente;
+        public new string name;
+
+
+        public override void getData()
+        {
+            name = "bruno";
+            base.name = "Marco";
+
+            Console.WriteLine(this.name); // Marco
+
+        }
+    }
+
+}
---
+++ poliformismo/Program.cs
@@ -0,0 +1,80 @@
+﻿
+using System;
+namespace poliformismo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Cliente person = new Cliente();
+            person.getData();
+        }
+
+    }
+    public class Person
+    {
+        public string name;
+        public int age { get; set; }
+
+        public virtual void getData()
+        {
+
+        }
+
+        public Person()
+        {
+
+        }
+    }
+
+    public class Cliente : Person
+    {
+        public string codiceCliente;
+        public new string name;
+        public Conto _conto { get; set; }
+
+        public override void getData()
+        {
+            name = "bruno";
+            base.name = "Marco";
+
+            Console.WriteLine(this.name); // Marco
+
+        }
+        public void AssociaConto(Conto conto)
+        {
+            _conto = conto;
+        }
+    }
+
+    public class Conto
+    {
+        bool isOpened = true;
+        public Cliente _cliente { get; set; }
+
+        public Banca _banca { get; set; }
+        public void associate()
+        {
+            _cliente.AssociaConto(this);
+        }
+        public Conto(Banca banca)
+        {
+            _banca = banca;
+        }
+    }
+
+    public class Banca
+    {
+        public Conto[] _conto { get; set; }
+
+
+        public void AddConto()
+        {
+            _conto[0] = new Conto(this);
+        }
+    }
+
+
+
+}
